e11a03c82741eb767fd9b9ed6b9f739b1a78a7d1
fumi1623 <ancient17.hirofumi@gmail.com>
2021-03-10 05:19:55 +0000

    [Upload]flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params2)
-    book.save
-    redirect_to books_path
+    if book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      flash.now[:alert] = "No created"
+      redirect_to books_path
+    end
   end
 
   def edit
